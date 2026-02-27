99cf5b2d6b625ce46e08a31798db80324a7a4ff4
MD. Saif Islam <muhammadsaif7717@gmail.com>
2026-02-27 06:00:43 +0600

    added next-auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,5 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import NextAuth from 'next-auth';
+import { authOptions } from '@/lib/authOptions';
 
-export const connectDb = async () => {
-  let db;
-  if (db) {
-    return db;
-  }
-
-  const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
-
-  if (!uri) {
-    throw new Error('Missing MongoDB URI');
-  }
-
-  try {
-    const client = new MongoClient(uri, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
-    });
-
-    const db = client.db('tulikona');
-    console.log('Connected to MongoDB successfully');
-    return db;
-  } catch (err) {
-    throw new Error(`Failed to connect to MongoDB ${err}`);
-  }
-};
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
